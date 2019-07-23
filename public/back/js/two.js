81e1111276367f1c00d4eb5f1dc683e136552716
weboldboy1994 <1223455394@qq.com>
2019-07-23 23:39:03 +0800

    好累啊 尼玛终于写的差不多了

--- public/back/js/two.js
+++ public/back/js/two.js
@@ -72,6 +72,9 @@ $(function(){//入口函数
             var id = $(this).attr("data-id")
             //  把id设置给input框
             $('[name="categoryId"]').val(id)
+
+            //将隐藏域校验状态，设置成校验成功状态  updateStatus(字段名，校验状态，校验规则)
+            $('#form').data('bootstrapValidator').updateStatus('categoryId','VALID');
       })
 
       //  05 进行文件上传初始化
@@ -89,11 +92,78 @@ $(function(){//入口函数
             //将图片地址设置给input
             $('[name="brandLogo"]').val(imgUrl)
 
+            //手动重置隐藏域的状态
+            $('#form').data('bootstrapValidator').updateStatus('brandLogo','VALID')
+
             }
-      
       });
 
+      //  06 实现表单校验
+      $('#form').bootstrapValidator({
+             //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
+            excluded: [],//需要对隐藏域进行校验，所以把它设置为空
+
+            //配置图标
+            feedbackIcons: {
+                  valid: 'glyphicon glyphicon-ok',  //校验成功
+                  invalid: 'glyphicon glyphicon-remove',   //校验失败
+                  validating: 'glyphicon glyphicon-refresh'  //校验中
+                },
+            //配置字段
+            fields:{
+                  //categoryId  分类id
+                  //brandName   二级分类名称
+                  //brandLogo   图片地址
+                  categoryId:{//配置校验规则
+                        validators:{
+                              notEmpty:{message:"请选择一级分类" }, //提示
+                        }
+                  },
+                  brandName:{
+                        validators:{
+                              notEmpty:{message:"请输入二级分类" }, //提示
+                        }
+                  },
+                  brandLogo:{
+                        validators:{
+                              notEmpty:{message:"请选择图片" }, //提示
+                        }
+                  }
+            }
+            
+      })
+
+
+      //07  注册表单校验成功事件，阻止默认提交，通过ajax进行提交
+      $("#form").on('success.form.bv', function (e) {
+            e.preventDefault();
+            //使用ajax提交逻辑
+            $.ajax({
+                  type:'post',
+                  url:"/category/addSecondCategory",
+                  dataType:'json',
+                  data:$('#form').serialize(),
+                  success:function(result){
+                        console.log(result)
+                  if(result.success){
+                        //关闭模态框
+                  $('#addModal').modal("hide")
+                        //重新渲染第一个页面
+                  currentPage=1
+                  render()
+                        //重置模态框表单,全部重置
+                  $('#form').data('bootstrapValidator').resetForm(true)
+                  //手动重置文本内容和图片路径
+                  $('#dorpdownText').text("请选择一级分类")
+                  $('#imgBox img').attr('src','images/none.png')
+                  
+                  }
+                  }
+            })
+        });
 })
+
+
 
 // 文件上传思路整理
 // 01 引包
